251e169c475b012b2362b25703f26fee122ae564
juan.almeyda <juan.almeyda@olx.com>
2021-08-09 23:31:19 +0200

    remove json library

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,6 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
-    implementation("com.eclipsesource.minimal-json:minimal-json:0.9.4")
-
     implementation(platform("org.http4k:http4k-bom:4.10.1.0"))
     implementation("org.http4k:http4k-core")
     implementation("org.http4k:http4k-server-apache")
