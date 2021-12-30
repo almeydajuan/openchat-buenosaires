c11bece0eadcac9ad7e508a35c91ff853568cfdc
Juan Almeyda <juan.almeyda@olx.com>
2021-12-30 12:45:11 +0100

    add can register user test

--- src/test/kotlin/com/almeydajuan/openchat/model/OpenChatSystemTest.kt
+++ src/test/kotlin/com/almeydajuan/openchat/model/OpenChatSystemTest.kt
@@ -13,5 +13,26 @@ internal class OpenChatSystemTest {
         assertEquals(0, system.numberOfUsers())
     }
 
+    @Test
+    fun `can register user`() {
+        val system = createSystem()
+        val registeredUser = registerPepeSanchez(system)
+
+        assertTrue(system.hasUsers())
+        assertTrue(system.hasUserNamed(TestObjectsBucket.PEPE_SANCHEZ_NAME))
+        assertEquals(1, system.numberOfUsers())
+        assertTrue(registeredUser.isNamed(TestObjectsBucket.PEPE_SANCHEZ_NAME))
+        assertEquals(TestObjectsBucket.PEPE_SANCHEZ_ABOUT, registeredUser.about)
+        assertEquals(TestObjectsBucket.PEPE_SANCHEZ_HOME_PAGE, registeredUser.homePage)
+        assertNotEquals(TestObjectsBucket.PEPE_SANCHEZ_HOME_PAGE + "x", registeredUser.homePage)
+    }
+
+    private fun registerPepeSanchez(system: OpenChatSystem): User = system.register(
+            TestObjectsBucket.PEPE_SANCHEZ_NAME,
+            TestObjectsBucket.PEPE_SANCHEZ_PASSWORD,
+            TestObjectsBucket.PEPE_SANCHEZ_ABOUT,
+            TestObjectsBucket.PEPE_SANCHEZ_HOME_PAGE
+    )
+
     private fun createSystem() = OpenChatSystem()
 }
--- src/test/kotlin/com/almeydajuan/openchat/model/TestObjectsBucket.kt
+++ src/test/kotlin/com/almeydajuan/openchat/model/TestObjectsBucket.kt
@@ -2,4 +2,7 @@ package com.almeydajuan.openchat.model
 
 object TestObjectsBucket {
       const val PEPE_SANCHEZ_NAME = "Pepe Sanchez"
+      const val PEPE_SANCHEZ_PASSWORD = "password"
+      const val PEPE_SANCHEZ_ABOUT = "anotherAbout"
+      const val PEPE_SANCHEZ_HOME_PAGE = "www.twitter.com/pepeSanchez"
 }
